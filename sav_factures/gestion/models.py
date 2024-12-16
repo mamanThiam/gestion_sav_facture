c661b837babaeb51a804b555f0bc05fd5ddf5509
maman thiam <mamanthiam31@gmail.com>
2024-12-16 15:39:11 +0000

    modélisation de la base de donnée avec django

---
+++ sav_factures/gestion/models.py
@@ -0,0 +1,75 @@
+from django.db import models
+from django.core.exceptions import ValidationError
+
+# Table Client
+class Client(models.Model):
+    nom = models.CharField(max_length=255)
+    adresse = models.TextField()
+    email = models.EmailField(unique=True)
+    telephone = models.CharField(max_length=15)
+
+    def clean(self):
+        if not self.telephone.isdigit():
+            raise ValidationError("Le numéro de téléphone doit contenir uniquement des chiffres.")
+# Table Ascenseur
+class Ascenseur(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name="ascenseurs")
+    modele = models.CharField(max_length=255)
+    marque = models.CharField(max_length=50)
+    numero_serie = models.CharField(max_length=255, unique=True)
+    charge = models.CharField(
+        max_length=15, 
+        choices=[
+            ('450', '450'), 
+            ('630', '630'), 
+            ('800', '800'), 
+            ('1000', '1000'), 
+            ('1500', '1500'), 
+            ('2000', '2000')
+        ]
+    )
+    date_installation = models.DateField()
+
+    def __str__(self):
+        return f"{self.marque} - {self.modele} - {self.numero_serie}"
+
+# Table Intervention
+class Intervention(models.Model):
+    ascenseur = models.ForeignKey(Ascenseur, on_delete=models.CASCADE, related_name="interventions")
+    technicien = models.CharField(max_length=255)
+    statut = models.CharField(
+        max_length=50, 
+        choices=[
+            ('En cours', 'En cours'), 
+            ('Terminé', 'Terminé')
+        ]
+    )
+    type_intervention = models.CharField(
+        max_length=15, 
+        choices=[
+            ('Entretien', 'Entretien'), 
+            ('Réparation', 'Réparation'), 
+            ('Diagnostic', 'Diagnostic')
+        ]
+    )
+    date = models.DateField()
+
+    def __str__(self):
+        return f"Intervention {self.type_intervention} sur {self.ascenseur}"
+
+# Table Facture
+class Facture(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name="factures")
+    description_services = models.TextField()
+    montant = models.DecimalField(max_digits=10, decimal_places=2)
+    statut = models.CharField(
+        max_length=15, 
+        choices=[
+            ('Payé', 'Payé'), 
+            ('Non payé', 'Non payé')
+        ]
+    )
+    date_emission = models.DateField()
+
+    def __str__(self):
+        return f"Facture #{self.id} - {self.client.nom} - {self.statut}"
